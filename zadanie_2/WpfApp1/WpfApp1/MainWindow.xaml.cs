956ec2852f6846e0358bf3ccc0809e543447058d
SEUENHWC <anatolylekarev@vk.com>
2024-10-18 09:31:58 +0500

    sdelal 2 zadanie

---
+++ zadanie_2/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textInput.FontSize = fontSizeSlider.Value;
+        }
+
+        private void FontSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (textInput != null)
+            {
+
+                textInput.FontSize = e.NewValue; 
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                Title = "Сохранить текст"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+
+                File.WriteAllText(saveFileDialog.FileName, textInput.Text);
+                MessageBox.Show("Текст успешно сохранен!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
